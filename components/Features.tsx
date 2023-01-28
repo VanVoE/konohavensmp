bf7b852be1a339bc55daa6f0611ac536a529f947
CodeVanimal <codevanimal@gmail.com>
2023-01-28 10:48:50 -0600

    Update Features.tsx

--- components/Features.tsx
+++ components/Features.tsx
@@ -30,7 +30,7 @@ const Features = () => {
             <div className='flex items-center bg-[rgba(0,0,0,.2)] py-2 px-4 montserrat rounded-md'> <MdEmojiEvents className='text-white text-lg mr-4'/> Events </div>
             <div className='flex items-center bg-[rgba(0,0,0,.2)] py-2 px-4 montserrat rounded-md'> <MdReviews className='text-white text-lg mr-4'/> Shop Reviews </div>
             <div className='flex items-center bg-[rgba(0,0,0,.2)] py-2 px-4 montserrat rounded-md'> <GiRolledCloth className='text-white text-lg mr-4'/> Fabric </div>
-            <div className='flex items-center bg-[rgba(0,0,0,.2)] py-2 px-4 montserrat rounded-md'> <GiGamepad className='text-white text-2xl mr-4'/> Version 1.19 </div>
+            <div className='flex items-center bg-[rgba(0,0,0,.2)] py-2 px-4 montserrat rounded-md'> <GiGamepad className='text-white text-2xl mr-4'/> Version 1.19.2 </div>
             <div className='flex items-center bg-[rgba(0,0,0,.2)] py-2 px-4 montserrat rounded-md'> <GiMinerals className='text-white text-2xl mr-4'/> Lithium </div>
             <div className='flex items-center bg-[rgba(0,0,0,.2)] py-2 px-4 montserrat rounded-md'> <FaMicrophoneAlt className='text-white text-lg mr-4'/> Simple Voice Chat </div>
             <div className='flex items-center bg-[rgba(0,0,0,.2)] py-2 px-4 montserrat rounded-md'> <BsFillMoonStarsFill className='text-white text-lg mr-4'/> Starlight </div>
